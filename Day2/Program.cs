44c76c9497e2060ed2593ec3643301b974050165
fvrkan <oguzlock@outlook.com>
2023-08-21 23:45:15 +0300

    Add project files.

---
+++ Day2/Program.cs
@@ -0,0 +1,141 @@
+﻿
+//Console.WriteLine("vize notunuz : ");
+//double vize = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("final notunuz : ");
+//double final = Convert.ToDouble(Console.ReadLine());
+
+//double ort = (vize * 4 / 10) + (final * 6 / 10);
+
+//Console.WriteLine("Ortalamanız : " + ort);
+
+//if (ort >= 50)
+//{
+//    Console.WriteLine("geçtiniz");
+//}
+//else if (ort == 100)
+//{
+//    Console.WriteLine("Tam puan aldınız");
+//}
+//else
+//{
+//    Console.WriteLine("kaldınız");
+//}
+//Console.WriteLine("1. sayı  : ");
+//double s1 = Convert.ToDouble(Console.ReadLine());
+
+//Console.WriteLine("İşleminiz  : ");
+//String @islem = (Console.ReadLine());
+
+//Console.WriteLine("2. sayı  : ");
+//double s2 = Convert.ToDouble(Console.ReadLine());
+
+//if (@islem == "+")
+//{
+//    Console.WriteLine(s1 + s2);
+//}
+//else if (@islem == "*")
+//{
+//    Console.WriteLine(s1 * s2);
+//}
+//else if (@islem == "/")
+//{
+//    Console.WriteLine(s1 / s2);
+//}
+//else if (@islem == "-")
+//{
+//    Console.WriteLine(s1 - s2);
+//}
+
+
+//String[] meyveler = { "elma", "armut", "ayva", "domates" };
+//foreach(var i in meyveler)
+//{
+//    Console.WriteLine(i);
+//}
+
+//int[] notlar = { 12, 45, 73, 21, 78 };
+//for (int i = 0; i < notlar.Length; i++)
+//{
+//    Console.WriteLine(notlar[i]);
+//}
+
+
+
+//List<int> numbers = new() { 1,2,3,4,5,6,7,8,9};
+//List<int> evennumbers = new();
+//List<int> oddnumbers = new();
+
+//// 1. yöntem
+//foreach (var number in numbers)
+//{
+//    if (number % 2 == 0)
+//    {
+//       evennumbers.Add(number);
+//    }
+//    else
+//    {
+//        evennumbers.Add(number);
+//    }
+//}
+////2. Yöntem 
+
+//oddnumbers = numbers.FindAll(o => o % 2 != 0);
+//evennumbers = numbers.FindAll(e => e % 2 != 0);     
+
+
+
+//foreach (var number in oddnumbers)
+//{
+//    Console.WriteLine(number);
+//}
+
+
+
+//// tek satırlık foreachi öğren anni :)
+///
+
+
+// sıcaklığa göre durum ( if-else)
+
+//Console.WriteLine("Sıcaklığınız : ");
+//double temp = Convert.ToDouble(Console.ReadLine());
+
+//if(temp < 0)
+//{
+//    Console.WriteLine("Katıdır");
+//}
+//else if (temp >0 || temp < 100)
+//{
+//    Console.WriteLine("Sıvıdır");
+//}
+//else
+//{
+//    Console.WriteLine("Gazdır");
+//}
+
+
+// while ;
+
+for(int i = 20; i > 0; i--)
+{
+    
+    if (i == 16)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+
+}
+
+
+//OOP
+
+public class ogrenci
+{
+
+}
+
+
+
+
---
+++ FirstExample/Program.cs
@@ -0,0 +1,113 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//***** ömerden sonraki *****
+
+
+// Değer tipler , Referans Tipler 
+// Değer tipler : bool (Doğruluk değeri tutan veri tipidir.)
+// int : sayısal değerler tutar.
+// Float veya Double : Ondalıklı sayı tutar.
+// Sayısal Değerler
+//int sayi1 = 10;
+//Console.WriteLine(sayi1);
+
+//bool
+//bool dogruluk = false;
+//bool dogruluk1= true;
+//Console.WriteLine(dogruluk1);
+//Console.WriteLine(dogruluk);
+// Değer Tiplerin Özellikleri
+// Hafızanın stack kısmında tutulur.
+// null değer alamazlar.
+//int? sayi2=null;
+//Console.WriteLine(sayi2);
+// Referans Tipler 
+// Hafızanın Heap kısmında tutulur
+// string, Array,HashMap,List,class, interface 
+
+
+//string metin = " İlk metin örneği";
+//Console.WriteLine(metin);
+//todo: iki tane dğişken olsun ve bu 2 değişkeni toplasın ekrana bastırsın. 
+int a = 10;
+int b = 15;
+int toplam = a + b;
+// 1.Senaryo
+Console.WriteLine(toplam);
+// 2. Senaryo 
+Console.WriteLine("iki sayının toplamı = " + a + "+" + b + "= " + toplam);
+// String interpolation
+Console.WriteLine($"iki sayının toplamı = {a} + {b} = {toplam}");
+//metin1metin2
+string metin1 = "metin1";
+string metin2 = "metin2";
+string birlestirilmisMetin = metin1 + metin2;
+Console.WriteLine(birlestirilmisMetin);
+
+//Stringden sonra int değer istersek 
+Console.WriteLine("İki sayının toplamı" + a + b);
+Console.WriteLine(a + b);
+
+
+char aa = 'A';
+Console.WriteLine((int)aa);
+
+string example = "Küçük Büyük veya String örneği";
+// Stringin içindei her karakteri büyük harf yapar.
+Console.WriteLine(example.ToUpper());
+//Stringin içindei her karakteri küçük harf yapar.
+Console.WriteLine(example.ToLower());
+// K harfi ile başlıyor mu
+Console.WriteLine(example.StartsWith("k"));
+
+// O harfi ile bitiyormu
+Console.WriteLine(example.EndsWith("O"));
+
+// metnin ilk 4 harfini ekrana bastırsın.
+Console.WriteLine(example[..4]);
+
+// metinin ilk 4 hanesinden swonraki bütün karakterleri getir
+Console.WriteLine(example[3..]);
+Console.WriteLine(example.GetType());
+
+// kod satırları sırasıyla toplama çıkarma bölme ve çarpma işlemleri yapsın sonuç olarak
+
+// iki sayının çarpımı ....
+// formül : kilo/ boy*boy
+
+Console.WriteLine("Boyunuz :");
+int boy = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Kilonuz ");
+int kilo = Convert.ToInt32(Console.ReadLine());
+
+
+double endex = kilo / Math.Pow(boy,2); // boy*boy
+
+
+
+// *** Dairenin Çevresini ve alanını hesaplayan kodları yazınız ***
+// alan : pi * r^2
+// çevre : 2*pi*r
+//  Console.WriteLine("yaricap : ");
+//  double r = Convert.ToDouble(Console.ReadLine());
+
+//  double pi = 3.14;
+
+//  double cevre = 2 * pi * r;
+//  Console.WriteLine("cevre :" + cevre);
+//  double alan = pi * r * r;
+//  Console.WriteLine("alan :" + alan);
+
+//Math Fonksiyonu ile
+double yaricap = 10.0;
+double pi = Math.PI;
+double alan = pi * Math.Pow(yaricap, 2);
+double cevre = 2 * pi * yaricap;
+Console.WriteLine($" yarıçapı : \n {yaricap} olan dairenin alanı : {alan}");
+Console.WriteLine("çevresi : " + cevre);
+
+Console.ReadLine();
+
+
